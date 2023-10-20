e180c05552484c2403f3a9d2f7bc6c5570abfd1d
Naum Milosheski <naum.milosheski@gmail.com>
2023-10-20 16:42:23 +0200

    refactor usePlatforms hook

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,15 @@
-import { Platform } from "../types/types";
+import { useQuery } from "@tanstack/react-query";
+import { FetchResponse, Platform } from "../types/types";
 import useData from "./useData";
+import apiClient from "../services/api-client";
 
-const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
-
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/lists/parents")
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24h
+  });
 export default usePlatforms;
