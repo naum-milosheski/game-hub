7fc5a676895f1feb53c404567bf1ac98cbd1001d
Naum Milosheski <naum.milosheski@gmail.com>
2023-10-20 20:20:24 +0200

    refactor-create lookup hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Box, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
-import NavBar from "./components/NavBar";
-import GameGrid from "./components/GameGrid";
-import GenreList from "./components/GenreList";
 import { useState } from "react";
-import { GameQuery, GenreType, Platform } from "./types/types";
+import GameGrid from "./components/GameGrid";
+import GameHeading from "./components/GameHeading";
+import GenreList from "./components/GenreList";
+import NavBar from "./components/NavBar";
 import PlatformSelector from "./components/PlatformSelector";
 import SortSelector from "./components/SortSelector";
-import GameHeading from "./components/GameHeading";
+import { GameQuery } from "./types/types";
 
 function App() {
   const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
